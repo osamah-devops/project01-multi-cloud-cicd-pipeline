05912ad387ce840be5c1eaca194faf53d7ce3dff
osamah-devops <osamah.devops@gmail.com>
2026-07-06 21:20:01 -0400

    Add frontend

--- infra/bastion.tf
+++ infra/bastion.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "bastion_key" {
   key_name   = "bastion-key"
-  public_key = base64encode(file("ec2-key.pub"))
+  public_key = filebase64("ec2-key.pub")
 }
 resource "aws_instance" "bastion" {
 
